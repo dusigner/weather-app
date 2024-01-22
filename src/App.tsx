4610b42f120240d9a623075001bb004d401655c4
Eduardo Soares <e.soares@samsung.com>
2024-01-22 14:17:39 -0300

    app weather

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
+// App.tsx
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import ForecastsPage from './pages/ForecastsPage';
+import AddForecastPage from './pages/AddForecastPage';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* Certifique-se de passar um array de locations vazio como propriedade para ForecastsPage */}
+        <Route path="/forecasts" element={<ForecastsPage locations={[]} />} />
+        <Route path="/add-forecast" element={<AddForecastPage />} />
+        {/* Adicione uma rota de redirecionamento para /add-forecast */}
+        <Route path="/" element={<Navigate to="/add-forecast" />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
